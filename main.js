f4272e778e92a2981291cc15d4406d86a3ce50e8
zedd <wangjidw@gmail.com>
2021-11-08 15:43:59 +0700

    update

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ server.use((req, res, next) => {
 // Use default router
 server.use(router)
 // start server
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
